21dd8c8fbdd7b69a234980c60f9c24649c720f43
Vishal <vbhosale909591@gmail.com>
2025-12-03 14:44:53 +0530

    Initial Spring Boot project commit

---
+++ src/main/java/com/example/demo/userController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class userController {
+    @GetMapping
+    public String displayString() {
+        return  "hello demo";
+    }
+    
+}
